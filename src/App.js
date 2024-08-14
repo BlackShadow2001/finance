3072371d1ffe265cb3c25d6c963778009744af9b
Rubin Raftter P <rubin@Rubins-MacBook-Air.local>
2024-08-14 11:30:03 +0530

    First push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Homepage from "./Components/Hompage/Homepage.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
